bab90fd140694af7d716c5c09e7956d478bd3c94
dongjun <basestar011@naver.com>
2022-02-10 00:12:00 +0900

    메인 페이지 추가

---
+++ src/components/LeagueTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-sheet outlined>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      caption="프리미어리그"
+      disable-sort
+      disable-filtering
+      disable-pagination
+      hide-default-footer
+      :loading="loading"
+      loading-text="로딩중!!"
+    ></v-data-table>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "LeagueTable",
+  data() {
+    return {
+      loading: true,
+      headers: [
+        {
+          text: "Dessert (100g serving)",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Calories", value: "calories" },
+        { text: "Fat (g)", value: "fat" },
+        { text: "Carbs (g)", value: "carbs" },
+        { text: "Protein (g)", value: "protein" },
+        { text: "Iron (%)", value: "iron" },
+      ],
+      items: [],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row>
+    <v-col cols="6">
+      <league-table></league-table>
+    </v-col>
+    <v-col cols="6">
+      <league-table></league-table>
+    </v-col>
+    <v-col cols="6">
+      <league-table></league-table>
+    </v-col>
+    <v-col cols="6">
+      <league-table></league-table>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import LeagueTable from "@/components/LeagueTable.vue";
+export default {
+  name: "MainPage",
+  components: {
+    LeagueTable,
+  },
+};
+</script>
+<style></style>
